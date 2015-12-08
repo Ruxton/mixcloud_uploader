a5a66ded6eb25842cc3b236ca36aa32faf97d5af
Ruxton <ruxton@thefrontiergroup.com.au>
2015-12-08 11:59:03 +0800

    Format response

--- mixcloud/response.go
+++ mixcloud/response.go
@@ -1,19 +1,19 @@
 package mixcloud
 
 type Response struct {
-  Details map[string][]string `json:"details,omitempty"`
-  Error *ErrorMessage `json:"error,omitempty"`
-  Result *ResponseResult `json:"result,omitempty"`
+	Details map[string][]string `json:"details,omitempty"`
+	Error   *ErrorMessage       `json:"error,omitempty"`
+	Result  *ResponseResult     `json:"result,omitempty"`
 }
 
 type ErrorMessage struct {
-  Message string `json:"message,omitempty"`
-  Type string `json:"type,omitempty"`
-  RetryAfter int `json:"retry_after,omitempty"`
+	Message    string `json:"message,omitempty"`
+	Type       string `json:"type,omitempty"`
+	RetryAfter int    `json:"retry_after,omitempty"`
 }
 
 type ResponseResult struct {
-  Key string `json:"key,omitempty"`
-  Message string `json:"message,omitempty"`
-  Success bool `json:"success",omitempty`
+	Key     string `json:"key,omitempty"`
+	Message string `json:"message,omitempty"`
+	Success bool   `json:"success",omitempty`
 }
